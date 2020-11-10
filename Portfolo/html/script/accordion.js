fc37726bd8665f51ad5f5d3be07b2ff33447d039
ByunEunKyung <66714725+ByunEunKyung@users.noreply.github.com>
2020-11-10 18:12:57 +0900

    Add files via upload

---
+++ Portfolo/html/script/accordion.js
@@ -0,0 +1,29 @@
+﻿$(function(){
+	var headers = ["H1","H2","H3","H4","H5","H6"];
+
+	$(".accordion").click(function(e) {  // 클래스 아코드언 클릭시 
+	  var target = e.target, // target 변수 = e.target // e는 앨리멘트 
+		  name = target.nodeName.toUpperCase(); // 네임=target.nodeName.toUpperCase();
+	  
+	  if($.inArray(name,headers) > -1) { // 
+		var subItem = $(target).next();
+		
+		//slideUp all elements (except target) at current depth or greater
+		var depth = $(subItem).parents().length; // 클릭하면 아래것들(부모요소가 포함한 것들)을 나타나게 하ㅣ기 위함 //length에 저장
+		var allAtDepth = $(".accordion p, .accordion div").filter(function() { // filter는 앞의 선택자에서 필터 조건에 맞는 제이쿼리 객체를 재 생성해줌
+		
+		  if($(this).parents().length >= depth && this !== subItem.get(0)) { 
+			return true;  // 트루라면 리턴 반복
+		  }
+		});
+		$(allAtDepth).slideUp("fast"); // 슬라이드 업 속도
+		
+		//slideToggle target content and adjust bottom border if necessary
+		// 아래 모양 모서리가 그대로 남도록 해줌
+		subItem.slideToggle("fast",function() {
+			$(".accordion :visible:last").css("border-radius","0 0 10px 10px");
+		});
+		$(target).css({"border-bottom-right-radius":"0", "border-bottom-left-radius":"0"});
+	  }
+	});
+});
---
+++ Portfolo/html/script/mod9.js
@@ -0,0 +1,74 @@
+﻿/* 슬라이드 	*/
+$(function(){
+	
+	$(function(){
+				$("#mainImg li img").each(function(aa){
+					$(this).css("left",aa*800);
+					});
+				});
+				
+				setInterval(function(){
+					$("#mainImg li img").each(function(){
+						var nowLeft = parseInt($(this).css("left"));
+							console.log($(this).css("left"));
+						var movePos = nowLeft -800;
+					$(this).animate({left:movePos}, 2000, function(){
+						if(movePos == -1600){
+							$(this).css("left", 800);
+						}
+						});
+					});
+					}, 2400);
+					
+		//메뉴 부분		
+	
+		$("#mainM2").mouseenter(function(){
+		$("li").children("#subM").stop().slideDown(500);
+		});
+	   $("#mainM2").mouseleave(function(){
+		$("li").children("#subM").stop().slideUp(500);
+	   });
+	   
+	   	$("#mainM3").mouseenter(function(){
+		$("li").children("#subM2").stop().slideDown(500);
+		});
+	   $("#mainM3").mouseleave(function(){
+		$("li").children("#subM2").stop().slideUp(500);
+	   });
+	   
+	   	$("#mainM4").mouseenter(function(){
+		$("li").children("#subM3").stop().slideDown(500);
+		});
+	   $("#mainM4").mouseleave(function(){
+		$("li").children("#subM3").stop().slideUp(500);
+	   });
+	   
+	// 버튼으로 페이지 이동
+	$(function(){
+ 
+		  $("header,#container").css({ marginTop:"0px" });
+		  $(".content_wrap").hide();
+		  $("header").animate({ marginTop:0 },function(){
+			   $("#container").animate({ marginTop:"80px" });
+		  });
+
+		  $("#buttonType li:eq(0)").click(function(){
+			$("html,body").animate({ scrollTop: 0* $(this).index() });
+		  });
+		  $("#buttonType li:eq(1)").click(function(){
+			$("html,body").animate({ scrollTop: 900* $(this).index() });
+		  });
+		  $("#buttonType li:eq(2)").click(function(){
+			$("html,body").animate({ scrollTop: 900* $(this).index() });
+		  });
+		  $("#buttonType li:eq(3)").click(function(){
+			$("html,body").animate({ scrollTop: 1500* $(this).index() });
+		  });
+
+		  $("#container>p").click(function(){
+			  $("html,body").animate({ scrollTop: 0 });
+		  });
+
+	});	
+
+});	   
